79a0fafa7d34c721d5a4dd14f01424a13c157d31
Habibullo09 <fayzullo.fa06@gmail.com>
2026-09-07 17:15:07 +0500

    fixupdatedTeachers

--- backend/migrations/20260817124423-updateAt.js
+++ backend/migrations/20260817124423-updateAt.js
@@ -3,18 +3,10 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn(
-      'Teachers',
-      'updatedAt',
-      {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.NOW
-      }
-    )
+  
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Teachers', 'updatedAt')
+   
   }
 };
