82ac822cd0790a7adf22a4ebf62da8a711ad033f
Habibullo09 <fayzullo.fa06@gmail.com>
2026-09-07 17:24:35 +0500

    fixupdaDateofbirthagain

---
+++ backend/migrations/20260907122240-fixingDateOfbirth.js
@@ -0,0 +1,28 @@
+'use strict';
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.changeColumn(
+      'Students',
+      "dateOfBirth",
+      {
+        type: Sequelize.DATEONLY,
+        allowNull: true
+      },
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.changeColumn(
+      "Students",
+      'dateOfBirth',
+      {
+          type: Sequelize.DATEONLY,
+        allowNull: false
+      }
+    )
+    
+  }
+};
