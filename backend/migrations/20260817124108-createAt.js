9249906b0243a1c56d0d14da6ccb8a56532a7749
Habibullo09 <fayzullo.fa06@gmail.com>
2026-09-07 17:11:24 +0500

    fix duplicate createdAt migration

--- backend/migrations/20260817124108-createAt.js
+++ backend/migrations/20260817124108-createAt.js
@@ -3,18 +3,9 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn(
-      'Teachers',
-      'createdAt',
-      {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.NOW
-      }
-    )
+   
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Teachers', 'createdAt')
-  }
+  
 };
